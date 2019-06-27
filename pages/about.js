86cc39bab0f8576e34a8d4cd518f207767e39824
ong-ar <reclusis@gmail.com>
2019-06-27 16:16:05 +0900

    next link, head 테스트

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import Head from "next/head";
+export default () => (
+  <div>
+    <Head>
+      <title>about</title>
+    </Head>
+    <h2>About page</h2>
+  </div>
+);
--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,14 @@
-export default () => <h1>hello world!!</h1>;
+import Link from "next/link";
+import Head from "next/head";
+
+export default () => (
+  <div>
+    <Head>
+      <title>index</title>
+    </Head>
+    <h1>hello world!!</h1>
+    <Link href={"/about"}>
+      <a>about</a>
+    </Link>
+  </div>
+);
