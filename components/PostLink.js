167088b580e3ed6983078f2f7889eb7a903b90e3
ong-ar <reclusis@gmail.com>
2019-06-27 17:09:27 +0900

    pretty urls 적용 (add express + next)

--- components/PostLink.js
+++ components/PostLink.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const PostLink = props => (
   <div>
-    <Link href={`/post?title=${props.title}`}>
+    <Link href={`/post?title=${props.title}`} as={`/post/${props.title}`}>
       <a>{props.title}</a>
     </Link>
   </div>
